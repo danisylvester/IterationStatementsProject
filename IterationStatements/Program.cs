547fa65c3bd80c3727e9a361bd7dd8853769763d
danisylvester <maria.sylvester10@gmail.com>
2020-08-26 12:05:57 -0500

    created program that adds int values to a list, prints list increasing in value and decreasing in value.

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace IterationStatements
 {
@@ -13,29 +14,44 @@ namespace IterationStatements
             var numbers = new List<int>();
             
             //Create a variable of type int and initialize the variable with a value of 0
-
+            var x = 0;
 
 
             // Create a do-while loop
+            do
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+                numbers.Add(++x); // Increment your variable by 1 // Then add your variable to "numbers"
             }
-            // While your variable is less than 100
+            while (x < 100) ;// While your variable is less than 100
+
+            Console.WriteLine($"Current value of x = {x}"); // checking value of x
 
 
 
             // Create a while loop
             // While your variable is less than 200
+            bool less200 = true;
+            while (less200)
             {
-                // Increment your variable by 1
-                // Then add your variable to "numbers"
+                if (x < 200)
+                {
+                    numbers.Add(++x); // Increment your variable by 1 // Then add your variable to "numbers"
+                }
+                else
+                    less200 = false;
             }
+            Console.WriteLine($"Current value of x = {x}"); // checking value of x
+
+
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
             // Write your variable to the console
+            foreach(int i in numbers)
+            {
+                Console.WriteLine($"{i}");
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
@@ -45,8 +61,10 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for(int i = 199; i <= numbers.Count && i >= 0; --i)
             {
                 // Write to the console "numbers" at index i
+                Console.WriteLine($"{numbers[i]}");
             }
         }
     }
